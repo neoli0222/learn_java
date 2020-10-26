9e7bc9640cf280273e566ef7e2d943fbc92b5ac4
lipengyong <neoli0222@gmail.com>
2020-10-26 23:25:49 +0800

    Fix typo in hello statement.

--- hello_world/src/com/github/neoli0222/learn/java/helloworld/Main.java
+++ hello_world/src/com/github/neoli0222/learn/java/helloworld/Main.java
@@ -1,8 +1,11 @@
 package com.github.neoli0222.learn.java.helloworld;
 
+/**
+ * @author neoli0222
+ */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world3!");
+        System.out.println("hello world!");
     }
 }
