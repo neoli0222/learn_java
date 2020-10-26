628cbbb64abdd490275fac1f27ece33bad28abcd
lipengyong <neoli0222@gmail.com>
2020-10-26 21:48:07 +0800

    Try to use idea to commit code

--- hello_world/src/com/github/neoli0222/learn/java/helloworld/Main.java
+++ hello_world/src/com/github/neoli0222/learn/java/helloworld/Main.java
@@ -3,6 +3,6 @@ package com.github.neoli0222.learn.java.helloworld;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world2!");
     }
 }
